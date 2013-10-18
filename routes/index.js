02027b0fe52a3fec4cf2442a6db0380260219d90
Kent C. Dodds <kent@doddsfamily.us>
2013-10-17 20:41:12 -0600

    Adding endpoint test.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 module.exports = function(app) {
   app.get('/api/*', function(req, res, next) {
-    console.log('hitting api');
     res.json({api: 'hit'});
   });
   app.get('*', function(req, res, next) {
--- test/server/appSpec.js
+++ test/server/appSpec.js
@@ -12,3 +12,14 @@ describe('App', function() {
     });
   });
 });
+
+describe('API', function() {
+  it('Should return an object', function(done) {
+    request(server).get('/api/anything').end(function(err, res) {
+      expect(res).to.exist;
+      expect(res.status).to.equal(200);
+      expect(res.body).to.eql({api: 'hit'});
+      done();
+    });
+  });
+});
