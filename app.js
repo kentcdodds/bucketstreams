43fd0fa4213e855c5704828e23dfbe1b1a27a9b2
Kent C. Dodds <kent@doddsfamily.us>
2013-10-19 23:18:16 -0600

    This debugging stuff is cramping my style...

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')();
 app.directory = __dirname;
 
 require('./config/express')(app);
-//require('./config/database')();
+require('./config/database')();
 require('./routes')(app);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+console.log('before app');
 var app = require('./app');
+console.log('after app');
 
 require('http').createServer(app).listen(app.get('port'), app.get('ip'), function () {
+  console.log('in create server');
   console.log('Express (' + app.get('env') + ') server listening on port ' + app.get('port'));
 });
