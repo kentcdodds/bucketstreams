7037e9e83274604f439d064d844e7ab049402176
Kent C. Dodds <kent@doddsfamily.us>
2013-10-20 00:08:43 -0600

    No wild card on the route...

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
   app.get('/api/*', function(req, res, next) {
     res.json({api: 'hit'});
   });
-  app.get('*', function(req, res, next) {
+  app.get('/', function(req, res, next) {
     res.render('index',{});
   });
 };
