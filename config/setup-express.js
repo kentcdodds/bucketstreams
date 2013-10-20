98cf838c3c4d50547d999db18a9e66e855d39c7a
Kent C. Dodds <kent@doddsfamily.us>
2013-10-19 23:26:59 -0600

    I'm thinking I don't understand how that config function works...

--- config/setup-express.js
+++ config/setup-express.js
@@ -5,46 +5,8 @@ console.log('required setup-express');
 module.exports = function(app) {
   console.log('export of setup-express');
   console.log('app id: ' + process.env.OPENSHIFT_APP_UUID);
-  var environmentConfig;
-  var environment;
 
-  if (process.env.OPENSHIFT_APP_UUID) {
-    environment = 'production';
-    environmentConfig = function productionSetup() {
-      app.set('port', process.env.OPENSHIFT_NODEJS_PORT);
-      app.set('ip', process.env.OPENSHIFT_NODEJS_IP);
-      app.set('views', path.join(app.directory, '/dist'));
-      app.use(express.static(app.directory + '/dist'));
-      app.use(express.cookieParser('Rock Run Slime George'));
-      app.use(express.static(path.join(app.directory, 'dist')));
-    };
-
-  } else {
-    environment = 'development';
-
-    environmentConfig = function developmentSetup() {
-      app.use(function staticsPlaceholder(req, res, next) {
-        return next();
-      });
-
-      app.set('port', process.env.PORT || 9000);
-      app.set('ip', process.env.IP || '127.0.0.1');
-      app.set('views', path.join(app.directory, '/app'));
-      app.use(express.static(app.directory + '/app'));
-      app.use(express.cookieParser('Toy Lion Story King'));
-
-      app.use(function middlewarePlaceholder(req, res, next) {
-        return next();
-      });
-
-      app.use(express.errorHandler());
-    };
-  }
-  app.configure(environment, function() {
-    console.log('Configuring ' + environment);
-    environmentConfig();
-
-    //Common stuff
+  var commonConfig = function() {
     app.engine('html', require('ejs').renderFile);
     app.set('view engine', 'html');
     app.use(express.favicon());
@@ -53,5 +15,36 @@ module.exports = function(app) {
     app.use(express.methodOverride());
     app.use(express.session());
     app.use(app.router);
+  };
+
+  app.configure('production', function() {
+    app.set('port', process.env.OPENSHIFT_NODEJS_PORT);
+    app.set('ip', process.env.OPENSHIFT_NODEJS_IP);
+    app.set('views', path.join(app.directory, '/dist'));
+    app.use(express.static(app.directory + '/dist'));
+    app.use(express.cookieParser('Rock Run Slime George'));
+    app.use(express.static(path.join(app.directory, 'dist')));
+    commonConfig();
   });
+
+  app.configure('development', function() {
+    app.use(function staticsPlaceholder(req, res, next) {
+      return next();
+    });
+
+    app.set('port', process.env.PORT || 9000);
+    app.set('ip', process.env.IP || '127.0.0.1');
+    app.set('views', path.join(app.directory, '/app'));
+    app.use(express.static(app.directory + '/app'));
+    app.use(express.cookieParser('Toy Lion Story King'));
+
+    app.use(function middlewarePlaceholder(req, res, next) {
+      return next();
+    });
+
+    app.use(express.errorHandler());
+    commonConfig();
+  });
+
+  console.log('Configuring finished');
 };
