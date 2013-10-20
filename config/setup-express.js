dc1d2c0dcf46a23a543d0faec85a80303dc60238
Kent C. Dodds <kent@doddsfamily.us>
2013-10-20 00:05:14 -0600

    Removed console.logs and added prod flag for simulating production locally.

--- config/setup-express.js
+++ config/setup-express.js
@@ -1,27 +1,22 @@
 var express = require('express');
 var path = require('path');
-console.log('required setup-express');
 
 module.exports = function(app) {
-  console.log('export of setup-express');
-  console.log('app id: ' + process.env.OPENSHIFT_APP_UUID);
-  console.log('environment: ' + process.env.NODE_ENV);
-  console.log('environment from express: ' + app.get('env'));
 
   var commonConfig = function() {
-    console.log('common config');
     app.engine('html', require('ejs').renderFile);
     app.set('view engine', 'html');
     app.use(express.favicon());
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
-    app.use(express.session());
+    if (!process.env.LOCAL) {
+      app.use(express.session());
+    }
     app.use(app.router);
   };
 
   app.configure('development', function() {
-    console.log('development config');
     app.use(function staticsPlaceholder(req, res, next) {
       return next();
     });
@@ -41,9 +36,8 @@ module.exports = function(app) {
   });
 
   app.configure('production', function() {
-    console.log('production config');
-    app.set('port', process.env.OPENSHIFT_NODEJS_PORT);
-    app.set('ip', process.env.OPENSHIFT_NODEJS_IP);
+    app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 9000);
+    app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
     app.set('views', path.join(app.directory, '/dist'));
     app.use(express.static(app.directory + '/dist'));
     app.use(express.cookieParser('Rock Run Slime George'));
@@ -51,5 +45,4 @@ module.exports = function(app) {
     commonConfig();
   });
 
-  console.log('Configuring finished');
 };
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-console.log('before app');
-var app = require('./app');
-console.log('after app');
+if (process.argv.indexOf('prod')) {
+  process.env.NODE_ENV = 'production';
+  process.env.LOCAL = true;
+}
 
-require('http').createServer(app).listen(app.get('port'), app.get('ip'), function () {
-  console.log('in create server');
-  console.log('Express (' + app.get('env') + ') server listening on port ' + app.get('port'));
+var app = require('./app');
+
+require('http').createServer(app).listen(app.get('port'), app.get('ip'), function() {
+  console.log('Express (' + app.get('env') + ') server listening here: ' + app.get('ip') + ':' + app.get('port'));
 });
