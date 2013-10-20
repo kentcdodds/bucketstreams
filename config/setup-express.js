517e0285694f47385f5b29fdf9a2ae1356a97f27
Kent C. Dodds <kent@doddsfamily.us>
2013-10-20 01:14:17 -0600

    Adding shrink-wrap. Hopefully this will make the build faster maybe?

--- config/setup-express.js
+++ config/setup-express.js
@@ -23,9 +23,7 @@ module.exports = function(app) {
 
     app.set('port', process.env.PORT || 9000);
     app.set('ip', process.env.IP || '127.0.0.1');
-    app.set('views', path.join(app.directory, '/app'));
     app.use(express.static(app.directory + '/app'));
-    app.use('/app', express.static(app.directory + '/app'));
     app.use(express.cookieParser('Toy Lion Story King'));
 
     app.use(function middlewarePlaceholder(req, res, next) {
@@ -39,12 +37,8 @@ module.exports = function(app) {
   app.configure('production', function() {
     app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 9000);
     app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-    app.set('views', path.join(app.directory, '/dist'));
     app.use(express.static(app.directory + '/dist'));
-    app.use('/dist', express.static(app.directory + '/dist'));
     app.use(express.cookieParser('Rock Run Slime George'));
-    app.use(express.static(path.join(app.directory, 'dist')));
-    app.use('/dist', express.static(path.join(app.directory, 'dist')));
     commonConfig();
   });
 
