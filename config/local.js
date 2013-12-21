9ae3fec2224ee1da7a5f1e4f250cbcc0648d167f
Kent C. Dodds <kent@doddsfamily.us>
2013-12-21 14:37:25 -0700

    Removing logs and setting the NODE_ENV variable.

--- config/local.js
+++ config/local.js
@@ -1,9 +1,6 @@
 var logger = require('winston');
 module.exports = function() {
-  logger.info(process.env.NODE_ENV + ' <-- Node env');
   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'local') {
-    process.env.NODE_ENV = 'local';
-    logger.info('Must be local!');
     require('../local/config')();
   }
 }
