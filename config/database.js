81b32f2332611e1131ce63bac9be4b8dc1f24f81
Kent C. Dodds <kent@doddsfamily.us>
2013-12-21 15:59:35 -0700

    Logging out the database string without the password

--- config/database.js
+++ config/database.js
@@ -7,11 +7,15 @@ module.exports = function() {
   require('../model');
 
   // Connect to database
+  var regex = /mongodb:\/\/.*?:(.*?)@.*?:.*?\/.*?$/;
+  logger.info('Connecting to mongo database: ' + process.env.MONGO_CONNECTION_STRING.replace(regex, function(full, pass) {
+    return full.replace(pass, '<<password-hidden>>');
+  }));
   mongoose.connect(process.env.MONGO_CONNECTION_STRING, function(err) {
     if (err) {
       logger.error('Error connecting to database!', err);
     } else {
-      logger.info('Connected to Mongo Database');
+      logger.info('Connected to database');
     }
   });
 
