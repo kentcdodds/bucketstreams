46106da444c64c8394d999f248728e5376aa1474
Kent C. Dodds <kent@doddsfamily.us>
2014-02-18 22:56:15 -0700

    Fixing the version of ui-bootstrap I'm using......

--- views/config.js
+++ views/config.js
@@ -33,7 +33,7 @@ module.exports = (function() {
       '/bower_components/momentjs/moment.js',
       '/bower_components/ng-file-upload/angular-file-upload.js',
       '/bower_components/angular-ui-router/release/angular-ui-router.js',
-      '/bower_components/angular-bootstrap/ui-bootstrap.js',
+      '/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       '/bower_components/angular-animate/angular-animate.js',
       '/bower_components/angular-resource/angular-resource.js',
       '/bower_components/angular-cookies/angular-cookies.js',
