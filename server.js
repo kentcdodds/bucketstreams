a55d47e4bd3cb9a36f7de43e2cd0975d6aa992f4
Kent C. Dodds <kent@doddsfamily.us>
2014-03-04 10:43:56 -0700

    Giving more info...

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ if (process.argv.indexOf('prod') > -1) {
 }
 
 if (process.env.OPENSHIFT_NODEJS_IP) {
-  console.log('On Openshift:', process.env.OPENSHIFT_NODEJS_IP + ':' + process.env.OPENSHIFT_NODEJS_PORT);
+  logger.info('On Openshift:', process.env.OPENSHIFT_NODEJS_IP + ':' + process.env.OPENSHIFT_NODEJS_PORT);
 }
 
 var app = require('./app');
+logger.info('Getting ready to create server... On ' + process.env.NODE_ENV + ' express (' + app.get('env') + ') server will listen here: ' + app.get('ip') + ':' + app.get('port'));
 
 require('http').createServer(app).listen(app.get('port'), app.get('ip'), function() {
   logger.info('On ' + process.env.NODE_ENV + ' express (' + app.get('env') + ') server listening here: ' + app.get('ip') + ':' + app.get('port'));
