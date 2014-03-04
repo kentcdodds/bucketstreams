28cbf5331c8b4fff19c3676278825bb26bc75e60
Kent C. Dodds <kent@doddsfamily.us>
2014-03-04 16:08:36 -0700

    RememberMe works on localStorage. Menu fixes.

--- app/front-page/FrontPageCtrl.js
+++ app/front-page/FrontPageCtrl.js
@@ -1,9 +1,23 @@
-angular.module('bs.frontPage').controller('FrontPageCtrl', function ($scope, $http, $window, User, AlertService) {
+angular.module('bs.frontPage').controller('FrontPageCtrl', function ($scope, $http, $window, User, AlertService, _) {
 
   $scope.password = '';
   $scope.email = '';
   $scope.going = false;
   $scope.action = 'Login';
+  var rememberMeKey = 'front-page-remember-me';
+  var rememberedUsername = localStorage.getItem(rememberMeKey);
+  $scope.rememberMe = !!rememberedUsername && !/null|undefined/.test(rememberedUsername);
+  if ($scope.rememberMe) {
+    $scope.username = rememberedUsername;
+  }
+  $scope.$watch('rememberMe + username', function() {
+    if ($scope.rememberMe && $scope.username) {
+      localStorage.setItem(rememberMeKey, $scope.username);
+    } else {
+      localStorage.removeItem(rememberMeKey);
+    }
+  });
+
 
   $scope.login = function(input, password) {
     loginOrRegister('login', input, password);
