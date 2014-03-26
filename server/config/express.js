b86ada044959b0459cc8570bbc36231672e57c8c
Kent C. Dodds <kent@doddsfamily.us>
2014-03-26 06:43:53 -0600

    Gotta let everyone in... This is local anyway so it should be ok :)

--- server/config/express.js
+++ server/config/express.js
@@ -34,7 +34,7 @@ module.exports = function(app) {
 
     //CORS middleware for development
     app.use(function(req, res, next) {
-      res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+      res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
       res.header('Access-Control-Allow-Headers', 'Content-Type');
 
